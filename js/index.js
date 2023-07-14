6f66bfb796e99b136a2e5b5f239fb4cb047184b3
ruben0032 <ruben.q002@gmail.com>
2023-07-14 13:43:07 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,8 +1,26 @@
 const $ = selector => document.querySelector(selector);
 const menu = $('.mobile-menu');
 const btnMenu = $('.mobile-menu-btn');
+const url = 'https://urgames-score.onrender.com/'
+
+const preloadProyect = async (urlPage) => {
+    try {
+        await fetch(urlPage,
+        {
+          method:'GET',
+          headers: { 'Content-Type': 'application/json' }
+        })
+    } catch (err) {
+        console.log(`Error: ${err}`);
+    }
+};
 
 btnMenu.addEventListener("click", (e) =>{
     e.preventDefault();
     menu.classList.toggle('hidden')
-})
+});
+
+window.addEventListener('load', async () => {
+    await preloadProyect(url);
+    console.log('pagina lista');
+});
